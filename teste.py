879cfe519eb3f0e79a3c2e2e861b04e7a08e2e50
Daniel-C-Martins <danicoxa.martins@outlook.com>
2024-03-07 19:19:57 -0300

    matriz_criada

--- teste.py
+++ teste.py
@@ -4,8 +4,16 @@ a = [[50], [50]];
 
 def main():
     os.system("cls")
-    arquivo = open("mapa.txt", "r")
-    
+
+    with open("Mapas\mapa50.txt", "r") as arquivo:
+        primeira_linha = arquivo.readline()
+        tamanho_matriz = primeira_linha[:2]
+        
+        a = [[tamanho_matriz],[tamanho_matriz]]
+
+        
+    # for linha in arquivo:
+    #   exit()     
 
 if __name__ == "__main__":
   main()
