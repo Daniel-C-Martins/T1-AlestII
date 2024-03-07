384bbf7c2520344f4a3ef588f6936fef28d87b4a
Daniel-C-Martins <danicoxa.martins@outlook.com>
2024-03-07 19:43:24 -0300

    leitura_mapa

--- teste.py
+++ teste.py
@@ -6,14 +6,18 @@ def main():
     os.system("cls")
 
     with open("Mapas\mapa50.txt", "r") as arquivo:
+        linhas = arquivo.readlines()
         primeira_linha = arquivo.readline()
         tamanho_matriz = primeira_linha[:2]
         
-        a = [[tamanho_matriz],[tamanho_matriz]]
-
-        
-    # for linha in arquivo:
-    #   exit()     
+        matriz = [[tamanho_matriz],[tamanho_matriz]]
+    
+    for linha in linhas:
+        teste = linha
+        matriz.append([testado for testado in teste])
+           
+    for i in matriz:
+        print(i)
 
 if __name__ == "__main__":
   main()
