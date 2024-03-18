facd77cea60b3d96435a68c9d033d799ea742ab4
Daniel Martins <martins.daniel23@edu.pucrs.br>
2024-03-18 13:32:31 -0300

    way_fix

--- workspace.py
+++ workspace.py
@@ -1,32 +1,35 @@
 import os
 
-#Variáveis globais importantes
+#Variáveis globais do mapa
 map = []
+location_x = 0
+location_y = 0
+
+#Variáveis globais de direção e controle 
+way = "right"
+end_controller = False
+
+#Variável globais relacionadas com dinheiro
 money = 0
+#count = "0"
 
 #Funções de leitura do mapa
-
 #Função responsável por ler o mapa.txt para uma matriz
 def read_map():
-    global map
-    with open("Maps\map500.txt", "r") as archive: #Leitura das linhas do arquivo txt para uma variável 
+    with open("Maps\map2000.txt", "r") as archive: #Leitura das linhas do arquivo txt para uma variável 
         archive_lines = archive.readlines()
-
-    for lines in archive_lines:    #"For" responsável por colocar as linhas na matriz
+        
+    for lines in archive_lines:         #"For" responsável por ler cada linha
         map_lines = []
-        for i in lines:             
+        for i in lines:                 #"For" responsável por ler cada caracter da linha para a lista             
             if i != "\n":
                 map_lines.append(i)     #"if" que nos permite fazer verificações na leitura
-        map.append(map_lines)
+        map.append(map_lines)       #Adiciona cada lista no mapa como uma linha
 
-#Função responsável por mostrar na tela a matriz
+#Função responsável por mostrar na tela o mapa
 def display_matrix(): 
-    for i in map:   #"For" simples para mostrar a matriz na tela
-        print("".join(i))
-
-#Função responsável por mostrar quanto dinheiro foi recuperado
-def display_saved_money():
-    print(money)
+    for line in map:   #"For" simples para mostrar o mapa na tela
+        print("".join(line))
 
 #Função responsável por encotrar onde se deve iniciar o caminhamento no mapa
 def find_first_location():
@@ -39,14 +42,18 @@ def find_first_location():
 
 #Função responsável por finalizar o programa
 def end(coord_x, coord_y):
-    if map[coord_x][coord_y] == "#": #Teste para saber se chegamos ao final do mapa 
-        display_saved_money()
-        exit()                              #Caso o teste seja verdadeiro, fecha o programa
+    if map[coord_x][coord_y] == "#": #Teste para saber se chegamos ao final do mapa
+        return True
+    else:
+        return False
+
+#Função responsável por mostrar quanto dinheiro foi recuperado
+def display_saved_money():
+    print(money)
 
 
 
-#Funções de verificação para as trocas de direção
-    
+#Funções de verificação para as trocas de direção    
 #Fumção responsável por testar se estamos caminhando por uma "\\"
 #O que indica que devemos mudar a direção do caminhamento
 def test_backslah(x,y):
@@ -64,121 +71,169 @@ def test_slash(x,y):
         return False
 
 
+# def conta_dinheiro(coord_x, coord_y):
+#     global count, money
+#     if map[coord_x][coord_y].isdigit():
+#         count += map[coord_x][coord_y]
+#     else:
+#         money += int(count)
+#         count = "0"
+
+# def testa_posicao_valida(coord_x, coord_y):
+#     if not map[coord_x][coord_y] == "":
+#         return False
+#     else: 
+#         return True
 
 #Funções para caminnhar na matriz
-
-#Função responsável por fazer o caminhamento para a direita na matriz         
+#Função responsável por fazer o caminhamento para a right na matriz         
 def move_right(coord_x, coord_y):
-    global money
+    global way, money, end_controller, location_x, location_y 
     count = "0"
+    print("andando right")
 
-    print("andando pra direita")
-
-    for i in range(coord_y, len(map[0])):  #Percorre a matriz para a direita apenas em Y(colunas)
-        end(coord_x, i)                   
-                                          
-        if map[coord_x][i].isdigit():  
-            count += map[coord_x][i]
-        else:
-            # if count != "0":
-            #     print(count)
-            money += int(count)
-            count = "0"
-
-        if test_slash(coord_x,i):
-            move_up(coord_x - 1, i)
-            break
-        if test_backslah(coord_x, i):
-            move_down(coord_x + 1, i)
+    for i in range(coord_y, len(map[1]) + 1):  #Percorre a matriz para a right apenas em Y(colunas)
+        if end(coord_x, i):
+            end_controller = True
             break
 
-#Função responsável por fazer o caminhamento para cima na matriz    
-def move_up(coord_x, coord_y):
-    global money
-    count = "0"
-    print("andando pra cima")
-    
-    for i in range(coord_x, 0, -1): #podemos otimizar
-        end(i, coord_y)
-            
-        if map[i][coord_y].isdigit():
-            count += map[i][coord_y]
-        else:
-            # if count != "0":
-            #     print(count)
-            money += int(count)
-            count = "0"
-
-        if test_backslah(i, coord_y):
-            move_left(i, coord_y - 1)
-            break
-        if test_slash(i, coord_y):
-            move_right(i, coord_y + 1)
-            break
-        
-#Função responsável por fazer o caminhamento para a esquerda na matriz           
-def move_left(coord_x, coord_y):
-    global money
-    count ="0"
-    print("andando pra esquerda")
-
-    for i in range(coord_y, 0, -1):
-        end(coord_x, i)
-            
         if map[coord_x][i].isdigit():
             count += map[coord_x][i]
         else:
-            # if count != "0":
-            #     print(count)
+            money += int(count)
+            count = "0"
+    
+        if test_slash(coord_x,i):
+            way = "up"
+            location_x = coord_x - 1
+            location_y = i
+            break
+        if test_backslah(coord_x, i):
+            way = "down"
+            location_x = coord_x + 1
+            location_y = i
+            break
+  
+#Função responsável por fazer o caminhamento para a left na matriz           
+def move_left(coord_x, coord_y):
+    global way, money, end_controller, location_x, location_y
+    print("andando left")
+    count = "0"
+
+    for i in range(coord_y, -1, -1):
+            
+        if end(coord_x, i):
+            end_controller = True
+            break
+        
+        if map[coord_x][i].isdigit():
+            count += map[coord_x][i]
+        else:
             money += int(count)
             count = "0"
 
+        
         if test_backslah(coord_x, i):
-            move_up(coord_x - 1, i)
+            way = "up"
+            location_x = coord_x - 1
+            location_y = i
             break
         if test_slash(coord_x, i):
-            move_down(coord_x + 1, i)
+            way = "down"
+            location_x = coord_x + 1
+            location_y = i
+            break 
+                           
+#Função responsável por fazer o caminhamento para up na matriz    
+def move_up(coord_x, coord_y):
+    global way, money, end_controller, location_x, location_y
+    print("andando up")
+    count = "0"
+    
+    for i in range(coord_x, -1, -1): 
+        if end(i, coord_y):
+          end_controller = True
+          break
+
+        if map[i][coord_y].isdigit():
+            count += map[i][coord_y]
+        else:
+            money += int(count)
+            count = "0"
+        
+        if test_slash(i, coord_y):
+            way = "right"
+            location_x = i
+            location_y = coord_y + 1
+            break
+        if test_backslah(i, coord_y):
+            way = "left"
+            location_x = i
+            location_y = coord_y - 1
             break
         
-#Função responsável por fazer o caminhamento para baixo na matriz    
-def move_down(coord_x, coord_y):
-    global money
-    count = "0"
-    print("andando pra baixo")
 
-    for i in range(coord_x, len(map[0])):
-        end(i, coord_y)
-            
+        
+#Função responsável por fazer o caminhamento para down na matriz    
+def move_down(coord_x, coord_y):
+    global way, money, end_controller, location_x, location_y
+    print("andando down")
+    count = "0"
+
+    for i in range(coord_x, len(map) + 1):
+        
+        if end(i, coord_y):
+            end_controller = True
+            break
+
         if map[i][coord_y].isdigit():
             count += (map[i][coord_y])
         else:
-            # if count != "0":
-            #     print(count)
             money += int(count)
             count = "0"
 
-        if test_backslah(i, coord_y):
-            move_right(i, coord_y + 1)
-            break
+        
         if test_slash(i, coord_y):
-            move_left(i, coord_y - 1)
+            way = "left"
+            location_x = i
+            location_y = coord_y - 1
             break
+        if test_backslah(i, coord_y):
+            way = "right"
+            location_x = i
+            location_y = coord_y + 1
+            break
+        
 
 
 
 # Função Main()
 def main():
-
+    global way, end_controller, location_x, location_y
     os.system("cls")
 
     read_map()
-    display_matrix()
+    #display_matrix()
     
-    first_location_x = find_first_location()
-    first_location_y = 0
+    location_x = find_first_location()
 
-    move_right(first_location_x, first_location_y)
-    print(money)
+    while(end_controller == False):
+      if way == "right":
+        move_right(location_x, location_y)
+      if way == "left":
+        move_left(location_x, location_y)
+      if way == "up":
+        move_up(location_x, location_y)
+      if way == "down":
+        move_down(location_x, location_y) 
+
+    print("Caminhamento concluido")
+    # print(len(map))
+    # print(len(map) + 1)
+    # print(len(map[0]))
+    # print(len(map[0][0]))
+    display_saved_money()
+    exit()
     
 if __name__ == "__main__":
   main()
